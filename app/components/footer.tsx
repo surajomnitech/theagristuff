6206aad80e13c93b40a65d5131a1d73f8d267abb
Suraj Prasanna <suraj145@gmail.com>
2024-12-06 17:31:45 +0530

    icon changes and social media links added

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => {
               <h3 className="text-xl font-bold mb-4">Follow Us</h3>
               <div className="flex justify-center md:justify-end space-x-4">
                 <a 
-                  href="#" 
+                  href="https://www.facebook.com/profile.php?id=100095362430794" 
                   className="text-gray-300 hover:text-white transition-colors"
                   aria-label="Facebook"
                 >
@@ -53,7 +53,7 @@ const Footer = () => {
                     <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
                   </svg>
                 </a>
-                <a 
+                {/* <a 
                   href="#" 
                   className="text-gray-300 hover:text-white transition-colors"
                   aria-label="Twitter"
@@ -80,7 +80,7 @@ const Footer = () => {
                   >
                     <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/>
                   </svg>
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
